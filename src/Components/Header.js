0bc303e7518caaadb5727ec5c678913719435f07
JunJie19 <jun_jie_1997@hotmail.com>
2021-11-06 17:45:43 +0000

    responsive desing

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Styles/head_foot.css'
 import { Link } from 'react-router-dom'
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Header() {
+    const [showLink, setshowLink] = useState(false)
+
     return (
-        <div className='header'>
+        <div className={showLink ? 'headerSmallScreen' : 'header'}>
             <div className='logo'><Link to='/'>Initial D</Link></div>
             <div className='navBar'>
                 <nav>
-                    <ul>
+                    <ul id={showLink ? 'hidden' : ''}>
                         <li><Link to='/'>Home</Link></li>
                         <li><Link to='gallery'>Gallery</Link></li>
                         <li><Link to='about'>About</Link></li>
                         <li><Link to='contact'>Contact</Link></li>
                     </ul>
+                    <button onClick={() => setshowLink(!showLink)}><MenuIcon /></button>
                 </nav>
             </div>
         </div>
